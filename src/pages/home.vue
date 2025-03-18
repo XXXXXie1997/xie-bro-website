37b697b1d0f459e13a61a399003a981544f4d0f8
XXXXXie1997 <1416849251@qq.com>
2025-03-18 17:22:52 +0800

    refactor:update homePage

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="container" :style="style">
-    <strong class="hello">{{ `${curTime}好，我小谢，希望你开心 🙋` }}</strong>
+    <strong class="hello">{{ `${curTime}好，我小谢 🙋` }}</strong>
     <div>首页的内容、样式和排版还在设计中，还请不要嫌弃🙇</div>
     <br />
 
-    <strong class="hello">下面简单说说我</strong>
+    <strong class="hello">简单说说我</strong>
     <div>
-      2015-2019年就读于天津农学院动物科学与动物医学学院，专业是设施畜牧业（养殖场规划设计一类的。。。）
+      我是一名拥有4年+开发经验的前端工程师，专注于企业级中后台系统与移动端应用的开发。技术栈以Vue生态为核心，熟练掌握Vue2/Vue3，能够高效完成复杂组件封装与性能优化。擅长通过TypeScript增强代码健壮性。
     </div>
     <br />
-    <div>在2018年了解并开始接触这个行业</div>
+    <div>
+      在多个项目中实践Uniapp跨端开发，并参与过移动端APP、微信小程序从需求分析到上线的全流程开发。
+    </div>
+    <br />
+    <div>
+      对数据可视化也有一定经验，基于ECharts开发过动态数据看板，利用AntV/S2封装交叉分析表格组件，通过AntV/X6实现ER关系图的展示交互，以及开发业务/审批流程画布等。
+    </div>
+    <br />
+    <div></div>
     <br />
 
     <div>
-      2021年初入行成为一名前端开发，至今已从业四年有余，没什么可圈可点的建树，基本是一直跟着公司步调走，少有时间实现自己的想法，现在看来也是有些可惜
+      四年来少有时间做自己想做的东西，希望2025能有一个新的开始，同时也希望你开心🎉
     </div>
-    <br />
-
-    <div>希望2025能有一个新的开始🎉</div>
   </div>
 </template>
 <script lang="ts" setup>
